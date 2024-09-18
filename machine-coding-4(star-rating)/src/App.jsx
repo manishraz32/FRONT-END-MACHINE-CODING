e3dee8c915b9f357983db0b33835683e8c41b6b1
Manish Kumar Prasad <manishraz32@gmail.com>
2024-09-18 20:06:08 +0530

    star rating question added

---
+++ machine-coding-4(star-rating)/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [maxStar, setMaxStar] = useState(5);
+  const [rating, setRating] = useState(3);
+  
+  const handleStarClick = (value) => {
+    setRating(value);
+  }
+
+  return (
+    <div className="main-container">
+    {Array(5).fill().map((_, index) => (
+      <img
+        onClick={() => handleStarClick(index+1)}
+        key={index} 
+        className="img"
+        src={index < rating ? "star.png" : "emptystar.svg"}
+        alt=""
+      />
+    ))}
+  </div>
+  
+  )
+}
+
+export default App
